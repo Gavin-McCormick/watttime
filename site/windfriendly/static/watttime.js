e6f6900722b819c65d03f80dba67a6ea08511d0d
ericnguyen <eric@ericnguyen.com>
2013-03-13 20:07:29 -0700

    Prototype of the wattTime mobile site.

---
+++ site/windfriendly/static/watttime.js
@@ -0,0 +1,98 @@
+// Globals. Don't judge.
+var forecast;
+
+// Utility function.
+function makeParams(paramsObj) {
+	var paramsArray = [];
+	$.each(paramsObj, function(key, value) {
+			paramsArray.push(key + '=' + value);
+	});
+  return paramsArray.join('&');
+};
+
+$(document).ready(function() {
+    // Get current mix info.
+		var url = 'http://watttime.beardedmaps.com/';
+		var mixParams = {
+			'callback': '?',
+			'ba': 'BPA'
+    };
+		$.getJSON(url + 'status?' + makeParams(mixParams), drawMix);
+		var forecastParams = {
+			'callback': '?',
+			'ba': 'BPA'
+    };
+		$.getJSON(url + 'forecast?' + makeParams(forecastParams), drawNextbest);
+		init();
+});
+
+function init() {
+  $('#waitbtn').click(function() {
+    $('#action').addClass('expandedpane');
+  });
+  $('#action input').click(function() {
+    $(this)[0].checked = false;
+    $('#action').removeClass('expandedpane');
+    $('#thanks').show().addClass('expandedpane');
+  });
+  $('#donebtn').click(function() {
+    $('#thanks').removeClass('expandedpane');
+    $('#thanks').delay(250).hide(1);
+  });
+  $('#getforecastbtn').click(function() {
+    var forecastEl = $('#forecast');
+    if (forecastEl.is(':visible')) {
+      $('#forecast').fadeOut(300);
+    } else {
+      $('#forecast').fadeIn(300);
+      drawChart();
+    }
+  });
+}
+
+function drawMix(data) {
+  var rainbow = new Rainbow(); 
+  rainbow.setNumberRange(0, 100);
+  rainbow.setSpectrum('000000', '99ff99');
+  var percentGreen = Math.round(data.percent_green);
+  var color = rainbow.colourAt(percentGreen);
+  mixnumber = $('#mixnumber');
+  mixnumber.text(percentGreen + '%').css({ 'color': color });
+}
+
+function drawNextbest(data) {
+  forecast = data.forecast;
+  var greenestHour = data.forecast[0];
+  $.each(data.forecast, function(index, hourForecast) {
+    if (hourForecast.percent_green > greenestHour.percent_green) {
+      greenestHour = hourForecast;
+    }
+  });
+  $('#getforecastbtn').text(greenestHour.hour + ' hours');
+}
+
+function drawChart() {
+  if (!forecast) return;
+
+  var forecastData = [['Hours from now', 'Percent Clean']];
+  $(forecast).each(function(index, forecastObj) {
+    forecastData.push([
+      forecastObj.hour + 'hrs',
+      forecastObj.percent_green
+    ]);
+  });
+  var data = google.visualization.arrayToDataTable(forecastData);
+
+  var options = {
+    title: '24-hour Power Mix Forecast',
+    backgroundColor: '#efe',
+    fontName: 'Quicksand',
+    titleTextStyle: {fontSize: 18, color: '#090'},
+    lineWidth: 4,
+    colors: ['#090'],
+    legend: {position: 'none'}
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('forecast'));
+  chart.draw(data, options);
+}
