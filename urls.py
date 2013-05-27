110df9f1e894dfa60919a7eb1fc9e287b7bcc70f
anna <anna@basil.local>
2013-05-26 20:21:16 -0700

    changed static urls

--- urls.py
+++ urls.py
@@ -44,6 +44,6 @@ urlpatterns +=  patterns('windfriendly.views',
 if settings.DEBUG:
   urlpatterns += patterns('',
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-      'document_root': settings.MEDIA_ROOT,
+      'document_root': settings.STATIC_ROOT,
     }),
   )
