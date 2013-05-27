1b746568f24daec3c9ad2607867195c607f9f553
anna <anna@basil.local>
2013-05-27 09:58:19 -0700

    use placeholder.html for pages

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-#from django.contrib.auth.decorators import login_required
 
 def faq(request):
-    return render(request, 'pages/faq.html')
+    return render(request, 'pages/placeholder.html', {'title': 'FAQ'})
 
 def contact(request):
-    return render(request, 'pages/contact.html')
+    return render(request, 'pages/placeholder.html', {'title': 'Contact WattTime'})
 
+def about_us(request):
+    return render(request, 'pages/placeholder.html', {'title': 'About WattTime'})
+
+def how_it_works(request):
+    return render(request, 'pages/placeholder.html', {'title': 'How WattTime works'})
 
