1217670b9cc131d6d56886151c3eee8dbf25d8b2
anna <annarschneider@gmail.com>
2013-06-03 10:25:48 -0700

    added sample about us text

--- pages/views.py
+++ pages/views.py
@@ -13,8 +13,8 @@ def faq(request):
 #    return render(request, 'pages/faq.html')
 
 def about_us(request):
-    return render(request, 'pages/placeholder.html', {'title': 'About WattTime'})
-#    return render(request, 'pages/about_us.html')
+ #   return render(request, 'pages/placeholder.html', {'title': 'About WattTime'})
+    return render(request, 'pages/about_us.html')
 
 def how_it_works(request):
     return render(request, 'pages/placeholder.html', {'title': 'How WattTime works'})
