622a3925a107b4fed8e5a1cb750cc58b35b080af
estansifer <eric.stansifer+github@gmail.com>
2013-06-24 21:42:02 -0700

    Shortened all text messages to around 135 characters or less.

--- accounts/messages.py
+++ accounts/messages.py
@@ -3,44 +3,46 @@
 import random
 
 def use_central_ac_message(marginal_fuel):
-	randint = random.randint(0,2)
-	if randint == 0:
-		return "WattTime Alert! Your power now clean %s. Hot out? You could pre-cool your house on clean power! Turn down temp a bit, put it back in 30 mins. Don't forget." % marginal_fuel
-	elif randint == 1:
-		return "WattTime Alert! You're now running on clean %s power. Won't last long! Can you think of something you could do to use power now instead of later?." % marginal_fuel
-	else:
-		return "WattTime Alert! Your power is really clean %s right now. Anything you can recharge now to use all that clean energy? Laptop? Phone?" % marginal_fuel
+    randint = random.randint(0,2)
+    if randint == 0:
+# characters counts:
+#              0         1         2         3         4         5         6         7         8         9         0         1         2         3
+        return "WattTime Alert! Your power is now clean %s. Hot out? You can precool your house on clean power! Turn down temp a bit for 30 mins." % marginal_fuel
+    elif randint == 1:
+        return "WattTime Alert! You're now running on clean %s power. Won't last long! Can you use that clean power now instead of later?" % marginal_fuel
+    else:
+        return "WattTime Alert! Your power is really clean %s right now. Anything you can recharge now to use all that clean energy? Laptop?" % marginal_fuel
 
 def use_message(marginal_fuel):
-	randint = random.randint(0,2)
-	if randint == 0:
-		return "WattTime Alert! Your power is unusually clean right now - %s. Can you avoid wasting that clean power? Great time to do laundry, or dishes! " % marginal_fuel
-	elif randint == 1:
-		return "WattTime Alert! You're now running on clean %s power. Won't last long! Can you think of something you could do to use power now instead of later?." % marginal_fuel
-	else:
-		return "WattTime Alert! Your power is really clean %s right now. Anything you can recharge now to use all that clean energy? Laptop? Phone?" % marginal_fuel
+    randint = random.randint(0,2)
+    if randint == 0:
+        return "WattTime Alert! Your power is unusually clean now - %s. Can you avoid wasting that clean power? Great time to do laundry or dishes!" % marginal_fuel
+    elif randint == 1:
+        return "WattTime Alert! You're now running on clean %s power. Won't last long! Can you use that clean power now instead of later?" % marginal_fuel
+    else:
+        return "WattTime Alert! Your power is really clean %s right now. Anything you can recharge now to use all that clean energy? Laptop?" % marginal_fuel
 
 def dont_use_central_ac_message(marginal_fuel):
-	randint = random.randint(0,3)
-	if randint == 0:
-		return "WattTime Alert! You're now getting dirty %s power. Help us shut them down, save some power now! Can you turn off your thermostat for 20 mins?" % marginal_fuel
-	elif randint == 1:
-		return "WattTime Alert! You're now running on %s. Help us shut them down by saving some power now! Think you could dial back the temperature 2 degrees for an hour?" % marginal_fuel
-	elif randint == 2:
-		return "WattTime Alert! Your power is from %s right now. Help us use less of that dirty energy source! Can you turn out an extra light?" % marginal_fuel
-	else:
-		return "WattTime Alert! You're now running on %s. Great time to procrastinate! Are you doing anything that uses power right now? Maybe you could take a 15 min break!" % marginal_fuel
+    randint = random.randint(0,3)
+    if randint == 0:
+        return "WattTime Alert! You're now getting dirty %s power. Help us shut them down, save some power now! Can you turn off your AC for 20 mins?" % marginal_fuel
+    elif randint == 1:
+        return "WattTime Alert! You're now running on %s. Help us shut them down! Think you could dial back the temperature 2 degrees for an hour?" % marginal_fuel
+    elif randint == 2:
+        return "WattTime Alert! Your power is from %s right now. Help us use less of that dirty energy source! Can you turn out an extra light?" % marginal_fuel
+    else:
+        return "WattTime Alert! You're now running on %s. Are you doing anything that uses power right now? Maybe you could take a 15 min break!" % marginal_fuel
 
 def dont_use_message(marginal_fuel):
-	randint = random.randint(0,3)
-	if randint == 0:
-		return "The marginal fuel is %s, so avoid using electricity if you can!" % marginal_fuel
-	elif randint == 1:
-		return "WattTime Alert! You're now running on %s. Great time to procrastinate! Are you doing anything that uses power right now? Maybe you could take a 15 min break!" % marginal_fuel
-	elif randint == 2:
-		return "WattTime Alert! Your power is from %s right now. Help us use less of that dirty energy source! Can you turn out an extra light?" % marginal_fuel
-	else:
-		return "WattTime Alert! You're now on dirty %s power. Great time to save energy! Anything charging that you could unplug now and put back later when power is cleaner?" % marginal_fuel
+    randint = random.randint(0,3)
+    if randint == 0:
+        return "The marginal fuel is %s, so avoid using electricity if you can!" % marginal_fuel
+    elif randint == 1:
+        return "WattTime Alert! You're now running on %s. Are you doing anything that uses power right now? Maybe you could take a 15 min break!" % marginal_fuel
+    elif randint == 2:
+        return "WattTime Alert! Your power is from %s right now. Help us use less of that dirty energy source! Can you turn out an extra light?" % marginal_fuel
+    else:
+        return "WattTime Alert! You're now on dirty %s power. Great time to save energy! Anything charging that you charge later on cleaner power?" % marginal_fuel
 
 def verify_phone_message(code):
     return "Hello from WattTime! Enter [%s] on the sign up page to verify your device. This is a 1-time message." % code
