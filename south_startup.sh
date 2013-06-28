1ada5bffe256fb726896b22ca188324c624b1d50
anna <annarschneider@gmail.com>
2013-06-27 22:00:10 -0700

    update south_startup to pip install south

--- south_startup.sh
+++ south_startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo pip install south
+python manage.py syncdb
 python manage.py migrate windfriendly 0001 --fake
 python manage.py migrate workers 0001 --fake
 python manage.py migrate allauth.socialaccount 0001 --fake
