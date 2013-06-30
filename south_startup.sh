c66a76c6d0a6177f0babce95e783eeaa8f7148b4
anna <annarschneider@gmail.com>
2013-06-29 21:42:06 -0700

    remove migrations from invitation

--- south_startup.sh
+++ south_startup.sh
@@ -4,7 +4,6 @@ sudo pip install south
 python manage.py syncdb
 python manage.py migrate windfriendly 0001 --fake
 python manage.py migrate workers 0001 --fake
-python manage.py migrate allauth.socialaccount 0012 --fake
+python manage.py migrate allauth.socialaccount 0001 --fake
 python manage.py migrate accounts 0001 --fake
 python manage.py migrate allauth.socialaccount.providers.facebook 0001 --fake
-pyython manage.py migrate invitation 0001 --fake
