6b43cba71ce266634fdbd942d5cbf5b75730095c
Gavin <gavin.mccormick@gmail.com>
2013-06-30 15:29:07 -0700

    Edited NE_status and invite_email

--- pages/views.py
+++ pages/views.py
@@ -11,102 +11,100 @@ from windfriendly.parsers import NEParser
 from settings import EMAIL_HOST_USER
 
 def faq(request):
-#    return render(request, 'pages/placeholder.html', {'title': 'FAQ'})
-    return render(request, 'pages/faq.html')
+#	 return render(request, 'pages/placeholder.html', {'title': 'FAQ'})
+	return render(request, 'pages/faq.html')
 
 def about_us(request):
- #   return render(request, 'pages/placeholder.html', {'title': 'About WattTime'})
-    return render(request, 'pages/about_us.html')
+ #	 return render(request, 'pages/placeholder.html', {'title': 'About WattTime'})
+	return render(request, 'pages/about_us.html')
 
 def how_it_works(request):
-#    return render(request, 'pages/placeholder.html', {'title': 'How WattTime works'})
-    return render(request, 'pages/how_it_works.html')
+#	 return render(request, 'pages/placeholder.html', {'title': 'How WattTime works'})
+	return render(request, 'pages/how_it_works.html')
 
 def terms_of_service(request):
-#    return render(request, 'pages/placeholder.html', {'title': 'Terms of Service'})
-    return render(request, 'pages/terms_of_service.html')
+#	 return render(request, 'pages/placeholder.html', {'title': 'Terms of Service'})
+	return render(request, 'pages/terms_of_service.html')
 
 def BPA_status(request):
-#    return render(request, 'pages/placeholder.html', {'title': 'Pacific Northwest current status'})
+#	 return render(request, 'pages/placeholder.html', {'title': 'Pacific Northwest current status'})
 	
 	 # compose message
-    message = "Hi Anna! This is as far as I got. Couldn't figure out how to pull BPA data after all. :-/ Gavin"
-    
-    return render(request, 'pages/BPA_status.html', {'marginal_message' : message})
+	message = "Hi Anna! This is as far as I got. Couldn't figure out how to pull BPA data after all. :-/ Gavin"
 	
-    return render(request, 'pages/BPA_status.html')
-	
+	return render(request, 'pages/BPA_status.html', {'marginal_message' : message})
+		
 def CA_status(request):
-#    return render(request, 'pages/placeholder.html', {'title': 'California current status'})
+#	 return render(request, 'pages/placeholder.html', {'title': 'California current status'})
 	
 	 # compose message
-    message = "Hi Anna! This is as far as I got. Couldn't figure out how to pull CAISO data after all. :-/ Gavin"
-    
-    return render(request, 'pages/CA_status.html', {'marginal_message' : message})
+	message = "Hi Anna! This is as far as I got. Couldn't figure out how to pull CAISO data after all. :-/ Gavin"
 	
-    return render(request, 'pages/CA_status.html')
-	
+	return render(request, 'pages/CA_status.html', {'marginal_message' : message})
+		
 def NE_status(request):
-#    return render(request, 'pages/placeholder.html', {'title': 'New England current status'})
+#	 return render(request, 'pages/placeholder.html', {'title': 'New England current status'})
 	# get current data
-    if NE.objects.count() == 0:
-        parser = NEParser()
-        parser.update()
+	if NE.objects.count() == 0:
+		parser = NEParser()
+		parser.update()
+	datum = NE.latest_point()	
+	print NE.latest_point(), NE.latest_date()
+	percent_green = datum.fraction_green() * 100.0
+	marginal_fuel = MARGINAL_FUELS[datum.marginal_fuel]
+	
+	 # compose message
+	greenery = str(int(percent_green + 0.5))
+	if marginal_fuel == 'None':
+		marginal_fuel = 'Mixed'
+	if marginal_fuel in ['Coal', 'Oil']:
+		message = "Right now in New England {p} percent of all power is coming from renewable energy. You can change this number! Right now any new power that's needed will come from {fuel}. That means this is a great time to SAVE energy."
+	elif marginal_fuel in ['Natural Gas', 'Refuse', 'Mixed']:
+		message = "Right now in New England {p} percent of all power is coming from renewable energy. Right now any new power that's needed will come from {fuel}. That means this is an AVERAGE time to use energy."
+	else:
+		message = "Right now in New England {p} percent of all power is coming from renewable energy. You can change this number! Right now any new power that's needed will come from {fuel}. That means this is a fine time to use MORE power."
+	message = message.format(p = greenery, fuel = marginal_fuel.lower())
+	return render(request, 'pages/NE_status.html', {'marginal_message' : message})
+
 
 def status_offline(request):
-    return render(request, 'pages/status.html', {'marginal_message' : 'Status is offline until July 1.'})
+	return render(request, 'pages/status.html', {'marginal_message' : 'Status is offline until July 1.'})
 
 def status(request):
-    datum = NE.objects.all().latest('date')
-    percent_green = datum.fraction_green() * 100.0
-    marginal_fuel = MARGINAL_FUELS[datum.marginal_fuel]
-	
-	 # compose message
-    greenery = str(int(percent_green + 0.5))
-    if marginal_fuel == 'None':
-        marginal_fuel = 'Mixed'
-    if marginal_fuel in ['Coal', 'Oil']:
-        message = "Right now in New England {p} percent of all power is coming from renewable energy. You can change this number! Right now any new power that's needed will come from {fuel}. That means this is a great time to SAVE energy."
-    elif marginal_fuel in ['Natural Gas', 'Refuse', 'Mixed']:
-        message = "Right now in New England {p} percent of all power is coming from renewable energy. Right now any new power that's needed will come from {fuel}. That means this is an AVERAGE time to use energy."
-    else:
-        message = "Right now in New England {p} percent of all power is coming from renewable energy. You can change this number! Right now any new power that's needed will come from {fuel}. That means this is a fine time to use MORE power."
-    message = message.format(p = greenery, fuel = marginal_fuel.lower())
-    return render(request, 'pages/NE_status.html', {'marginal_message' : message})
-	
-    return render(request, 'pages/NE_status.html')
+
+		return render(request, 'pages/status.html', {'marginal_message' : message})
 	
 def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
 
-            if cd['subject'] and cd['message'] and cd['email']:
-                try:
-                    send_mail(
-                        cd['subject'],
+			if cd['subject'] and cd['message'] and cd['email']:
+				try:
+					send_mail(
+						cd['subject'],
 			cd['email'] + '\n' + cd['message'],
 			cd['email'],
 			[EMAIL_HOST_USER],
 			fail_silently=False,
-                        )
-                    url = reverse('contact_thank_you')
-                    return HttpResponseRedirect(url)
-                except BadHeaderError:
-                    return HttpResponse('Invalid header found.')
-            else:
-                return HttpResponse('Make sure all fields are entered.')
-    else:
-        form = ContactForm()
+						)
+					url = reverse('contact_thank_you')
+					return HttpResponseRedirect(url)
+				except BadHeaderError:
+					return HttpResponse('Invalid header found.')
+			else:
+				return HttpResponse('Make sure all fields are entered.')
+	else:
+		form = ContactForm()
 
-    return render_to_response('pages/contact.html', {'form':form}, RequestContext(request))
+	return render_to_response('pages/contact.html', {'form':form}, RequestContext(request))
 
 def thankyou(request):
-    return render_to_response('pages/contact_thank_you.html')
-    
+	return render_to_response('pages/contact_thank_you.html')
+	
 def facebook_pilot(request):
-    return render(request, 'pages/facebook_pilot.html')
-    
+	return render(request, 'pages/facebook_pilot.html')
+	
 def sierra_pilot(request):
-    return render(request, 'pages/sierra_pilot.html')
+	return render(request, 'pages/sierra_pilot.html')
--- windfriendly/models.py
+++ windfriendly/models.py
@@ -4,248 +4,248 @@ from django.utils.timezone import now
 
 # Approximately in order from bad to good
 MARGINAL_FUELS = ['Coal', 'Oil', 'Natural Gas', 'Refuse', 'Hydro', 'Wood',
-                  'Nuclear', 'Solar', 'Wind', 'None']
+				  'Nuclear', 'Solar', 'Wind', 'None']
 FORECAST_CODES = {'ACTUAL': 0, 'actual': 0,
-                  'DAM': 1, 'dy_ahead': 1,
-                  'HASP': 2, 'hr_ahead': 2,
-                  'RTM': 3, 'mn_ahead': 3,
-                  }
+				  'DAM': 1, 'dy_ahead': 1,
+				  'HASP': 2, 'hr_ahead': 2,
+				  'RTM': 3, 'mn_ahead': 3,
+				  }
 
 class DebugMessage(models.Model):
-    date = models.DateTimeField(db_index=True)
-    message = models.CharField(max_length=300)
+	date = models.DateTimeField(db_index=True)
+	message = models.CharField(max_length=300)
 
 def debug(message):
-    dm = DebugMessage()
-    dm.date = now()
-    dm.message = message
-    dm.save()
+	dm = DebugMessage()
+	dm.date = now()
+	dm.message = message
+	dm.save()
 
 class CAISO(models.Model):
-    # load, wind, solar in MW
-    load = models.FloatField()
-    wind = models.FloatField()
-    solar = models.FloatField()
-    
-    # forecast type is the index in FORECAST_CODES
-    forecast_code = models.IntegerField()
-    
-    # date is local time at which these values will be true (can be in the future)
-    date = models.DateTimeField(db_index=True)
-    # date_extracted is the UTC time at which these values were pulled from CAISO
-    date_extracted = models.DateTimeField(db_index=True)
+	# load, wind, solar in MW
+	load = models.FloatField()
+	wind = models.FloatField()
+	solar = models.FloatField()
+	
+	# forecast type is the index in FORECAST_CODES
+	forecast_code = models.IntegerField()
+	
+	# date is local time at which these values will be true (can be in the future)
+	date = models.DateTimeField(db_index=True)
+	# date_extracted is the UTC time at which these values were pulled from CAISO
+	date_extracted = models.DateTimeField(db_index=True)
 
-    def total_load(self):
-        return self.load
-        
-    def fraction_green(self):
-        return (self.wind + self.solar) / self.load
-        
-    def fraction_high_carbon(self):
-        return 1.0 - self.fraction_green()
+	def total_load(self):
+		return self.load
+		
+	def fraction_green(self):
+		return (self.wind + self.solar) / self.load
+		
+	def fraction_high_carbon(self):
+		return 1.0 - self.fraction_green()
 
-    @property
-    def marginal_fuel(self):
-        return MARGINAL_FUELS.index('None')
-        
-    @classmethod
-    def latest_date(cls, forecast_type=None):
-        """Return most recent stored datetime for forecast type"""
-        try:
-            latest = cls.latest_point(forecast_type)
-            return latest.date
-        except:
-            return None
-        
-    @classmethod
-    def latest_point(cls, forecast_type=None):
-        """Return most recent stored data point for forecast type"""
-        if forecast_type is None:
-            forecast_type = 'actual'
-        forecast_code = FORECAST_CODES[forecast_type]
-        forecast_qset = cls.objects.filter(forecast_code=forecast_code)
-        try:
-            latest = forecast_qset.order_by('-date')[0]
-            return latest
-        except:
-            return None
-            
-    @classmethod
-    def earliest_date(cls, forecast_type=None):
-        """Return oldest stored datetime for forecast type"""
-        try:
-            earliest = cls.earliest_point(forecast_type)
-            return earliest.date
-        except:
-            return None
+	@property
+	def marginal_fuel(self):
+		return MARGINAL_FUELS.index('None')
+		
+	@classmethod
+	def latest_date(cls, forecast_type=None):
+		"""Return most recent stored datetime for forecast type"""
+		try:
+			latest = cls.latest_point(forecast_type)
+			return latest.date
+		except:
+			return None
+		
+	@classmethod
+	def latest_point(cls, forecast_type=None):
+		"""Return most recent stored data point for forecast type"""
+		if forecast_type is None:
+			forecast_type = 'actual'
+		forecast_code = FORECAST_CODES[forecast_type]
+		forecast_qset = cls.objects.filter(forecast_code=forecast_code)
+		try:
+			earliest = forecast_qset.order_by('-date')[0]
+			return earliest
+		except:
+			return None
+			
+	@classmethod
+	def earliest_date(cls, forecast_type=None):
+		"""Return oldest stored datetime for forecast type"""
+		try:
+			earliest = cls.earliest_point(forecast_type)
+			return earliest.date
+		except:
+			return None
 
-    @classmethod
-    def earliest_point(cls, forecast_type=None):
-        """Return oldest stored data point for forecast type"""
-        if forecast_type is None:
-            forecast_type = 'actual'
-        forecast_code = FORECAST_CODES[forecast_type]
-        forecast_qset = cls.objects.filter(forecast_code=forecast_code)
-        try:
-            earliest = forecast_qset.order_by('date')[0]
-            return earliest
-        except:
-            return None
-                    
-    @classmethod
-    def points_in_date_range(cls, starttime, endtime, forecast_type=None):
-        """Return all data ponits in the date range for forecast type"""
-        if forecast_type is None:
-            forecast_type = 'actual'
-        forecast_code = FORECAST_CODES[forecast_type]
-        forecast_qset = cls.objects.filter(forecast_code=forecast_code)
-        try:
-            points = forecast_qset.filter(date__range=(starttime, endtime))
-            return points
-        except:
-            return []
-                        
+	@classmethod
+	def earliest_point(cls, forecast_type=None):
+		"""Return oldest stored data point for forecast type"""
+		if forecast_type is None:
+			forecast_type = 'actual'
+		forecast_code = FORECAST_CODES[forecast_type]
+		forecast_qset = cls.objects.filter(forecast_code=forecast_code)
+		try:
+			earliest = forecast_qset.order_by('date')[0]
+			return earliest
+		except:
+			return None
+					
+	@classmethod
+	def points_in_date_range(cls, starttime, endtime, forecast_type=None):
+		"""Return all data ponits in the date range for forecast type"""
+		if forecast_type is None:
+			forecast_type = 'actual'
+		forecast_code = FORECAST_CODES[forecast_type]
+		forecast_qset = cls.objects.filter(forecast_code=forecast_code)
+		try:
+			points = forecast_qset.filter(date__range=(starttime, endtime))
+			return points
+		except:
+			return []
+						
 
 class BPA(models.Model):
-    """Raw BPA data"""
-    load = models.IntegerField()
-    wind = models.IntegerField()
-    thermal = models.IntegerField()
-    hydro = models.IntegerField()
-    date = models.DateTimeField(db_index=True)
+	"""Raw BPA data"""
+	load = models.IntegerField()
+	wind = models.IntegerField()
+	thermal = models.IntegerField()
+	hydro = models.IntegerField()
+	date = models.DateTimeField(db_index=True)
 
-    def get_title(self):
-        return unidecode(self.wind)
+	def get_title(self):
+		return unidecode(self.wind)
 
-    def total_load(self):
-        return float(self.wind + self.hydro + self.thermal)
+	def total_load(self):
+		return float(self.wind + self.hydro + self.thermal)
 
-    def fraction_green(self):
-        return self.wind / self.total_load()
+	def fraction_green(self):
+		return self.wind / self.total_load()
 
-    def fraction_high_carbon(self):
-        return self.thermal / self.total_load()
+	def fraction_high_carbon(self):
+		return self.thermal / self.total_load()
 
-    @property
-    def marginal_fuel(self):
-        return MARGINAL_FUELS.index('None')
+	@property
+	def marginal_fuel(self):
+		return MARGINAL_FUELS.index('None')
 
-    @classmethod
-    def latest_date(cls, forecast_type=None):
-        """Return most recent stored datetime for forecast type"""
-        try:
-            latest = cls.latest_point(forecast_type)
-            return latest.date
-        except:
-            return None
-        
-    @classmethod
-    def latest_point(cls, forecast_type=None):
-        """Return most recent stored data point for forecast type"""
-        forecast_qset = cls.objects.all()
-        try:
-            latest = forecast_qset.order_by('-date')[0]
-            return latest
-        except:
-            return None
-            
-    @classmethod
-    def earliest_date(cls, forecast_type=None):
-        """Return oldest stored datetime for forecast type"""
-        try:
-            earliest = cls.earliest_point(forecast_type)
-            return earliest.date
-        except:
-            return None
+	@classmethod
+	def latest_date(cls, forecast_type=None):
+		"""Return most recent stored datetime for forecast type"""
+		try:
+			latest = cls.latest_point(forecast_type)
+			return latest.date
+		except:
+			return None
+		
+	@classmethod
+	def latest_point(cls, forecast_type=None):
+		"""Return most recent stored data point for forecast type"""
+		forecast_qset = cls.objects.all()
+		try:
+			latest = forecast_qset.order_by('-date')[0]
+			return latest
+		except:
+			return None
+			
+	@classmethod
+	def earliest_date(cls, forecast_type=None):
+		"""Return oldest stored datetime for forecast type"""
+		try:
+			earliest = cls.earliest_point(forecast_type)
+			return earliest.date
+		except:
+			return None
 
-    @classmethod
-    def earliest_point(cls, forecast_type=None):
-        """Return oldest stored data point for forecast type"""
-        forecast_qset = cls.objects.all()
-        try:
-            earliest = forecast_qset.order_by('date')[0]
-            return earliest
-        except:
-            return None
+	@classmethod
+	def earliest_point(cls, forecast_type=None):
+		"""Return oldest stored data point for forecast type"""
+		forecast_qset = cls.objects.all()
+		try:
+			earliest = forecast_qset.order_by('date')[0]
+			return earliest
+		except:
+			return None
 
-    @classmethod
-    def points_in_date_range(cls, starttime, endtime, forecast_type=None):
-        """Return all data ponits in the date range for forecast type"""
-        forecast_qset = cls.objects.all()
-        try:
-            points = forecast_qset.filter(date__range=(starttime, endtime))
-            return points
-        except:
-            return []                               
-            
+	@classmethod
+	def points_in_date_range(cls, starttime, endtime, forecast_type=None):
+		"""Return all data ponits in the date range for forecast type"""
+		forecast_qset = cls.objects.all()
+		try:
+			points = forecast_qset.filter(date__range=(starttime, endtime))
+			return points
+		except:
+			return []								
+			
 # All units are megawatts
 class NE(models.Model):
-    gas = models.FloatField()
-    nuclear = models.FloatField()
-    hydro = models.FloatField()
-    coal = models.FloatField()
-    other_renewable = models.FloatField()
-    other_fossil = models.FloatField()
-    marginal_fuel = models.IntegerField()
-    date = models.DateTimeField(db_index=True)
+	gas = models.FloatField()
+	nuclear = models.FloatField()
+	hydro = models.FloatField()
+	coal = models.FloatField()
+	other_renewable = models.FloatField()
+	other_fossil = models.FloatField()
+	marginal_fuel = models.IntegerField()
+	date = models.DateTimeField(db_index=True)
 
-    def total_load(self):
-        return float(self.gas + self.nuclear + self.hydro + self.coal + self.other_renewable + self.other_fossil)
+	def total_load(self):
+		return float(self.gas + self.nuclear + self.hydro + self.coal + self.other_renewable + self.other_fossil)
 
-    def fraction_green(self):
-        return (self.hydro + self.other_renewable) / self.total_load()
+	def fraction_green(self):
+		return (self.hydro + self.other_renewable) / self.total_load()
 
-    def fraction_high_carbon(self):
-        return (self.coal) / self.total_load()
+	def fraction_high_carbon(self):
+		return (self.coal) / self.total_load()
 
-    @classmethod
-    def latest_date(cls, forecast_type=None):
-        """Return most recent stored datetime for forecast type"""
-        try:
-            latest = cls.latest_point(forecast_type)
-            return latest.date
-        except:
-            return None
-        
-    @classmethod
-    def latest_point(cls, forecast_type=None):
-        """Return most recent stored data point for forecast type"""
-        forecast_qset = cls.objects.all()
-        try:
-            latest = forecast_qset.order_by('-date')[0]
-            return latest
-        except:
-            return None
-            
-    @classmethod
-    def earliest_date(cls, forecast_type=None):
-        """Return oldest stored datetime for forecast type"""
-        try:
-            earliest = cls.earliest_point(forecast_type)
-            return earliest.date
-        except:
-            return None
+	@classmethod
+	def latest_date(cls, forecast_type=None):
+		"""Return most recent stored datetime for forecast type"""
+		try:
+			latest = cls.latest_point(forecast_type)
+			return latest.date
+		except:
+			return None
+		
+	@classmethod
+	def latest_point(cls, forecast_type=None):
+		"""Return most recent stored data point for forecast type"""
+		forecast_qset = cls.objects.all()
+		try:
+			latest = forecast_qset.order_by('-date')[0]
+			return latest
+		except:
+			return None
+			
+	@classmethod
+	def earliest_date(cls, forecast_type=None):
+		"""Return oldest stored datetime for forecast type"""
+		try:
+			earliest = cls.earliest_point(forecast_type)
+			return earliest.date
+		except:
+			return None
 
-    @classmethod
-    def earliest_point(cls, forecast_type=None):
-        """Return oldest stored data point for forecast type"""
-        forecast_qset = cls.objects.all()
-        try:
-            earliest = forecast_qset.order_by('date')[0]
-            return earliest
-        except:
-            return None
+	@classmethod
+	def earliest_point(cls, forecast_type=None):
+		"""Return oldest stored data point for forecast type"""
+		forecast_qset = cls.objects.all()
+		try:
+			earliest = forecast_qset.order_by('date')[0]
+			return earliest
+		except:
+			return None
  
-    @classmethod
-    def points_in_date_range(cls, starttime, endtime, forecast_type=None):
-        """Return all data ponits in the date range for forecast type"""
-        forecast_qset = cls.objects.all()
-        try:
-            points = forecast_qset.filter(date__range=(starttime, endtime))
-            return points
-        except:
-            return []                                   
-                              
+	@classmethod
+	def points_in_date_range(cls, starttime, endtime, forecast_type=None):
+		"""Return all data ponits in the date range for forecast type"""
+		forecast_qset = cls.objects.all()
+		try:
+			points = forecast_qset.filter(date__range=(starttime, endtime))
+			return points
+		except:
+			return []									
+							  
 class Normalized(models.Model):
   balancing_authority = models.CharField(max_length=100)
   total_watts = models.IntegerField() # capacity
@@ -254,8 +254,8 @@ class Normalized(models.Model):
   date = models.DateTimeField(db_index=True)
 
 class User(models.Model):
-    # name
-    name = models.CharField(max_length=100)
+	# name
+	name = models.CharField(max_length=100)
 
 class MeterReading(models.Model):
   # user id
@@ -274,7 +274,7 @@ class MeterReading(models.Model):
   cost = models.FloatField()
 
   def total_kwh(self):
-    return self.energy/3600.0 * self.duration
+	return self.energy/3600.0 * self.duration
   
   def total_cost(self):
-    return self.cost * self.duration / 3600.0 / 10000.0
+	return self.cost * self.duration / 3600.0 / 10000.0
