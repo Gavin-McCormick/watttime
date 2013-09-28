a3160851104a740434d8bef91ee7cfe320dc41e8
nabiber <nabib@savveo.com>
2013-09-28 13:53:54 -0700

    remove authapp

--- authapp/authapp/allauth/socialaccount/providers/openid/forms.py
+++
@@ -1,7 +0,0 @@
-
-from django import forms
-
-
-class LoginForm(forms.Form):
-    openid = forms.URLField(label=('OpenID'),
-                            help_text='Get an <a href="http://openid.net/get-an-openid/">OpenID</a>')
